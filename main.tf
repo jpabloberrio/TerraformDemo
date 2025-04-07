3a6f91dd9d987bce631b74edf59291c0f738f34e
Juan Pablo Berrío López <jpabloberrio@users.noreply.github.com>
2025-04-06 22:06:04 -0500

    wip

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
 module "vpc" {
-  source      = "./modules/vpc"
-  vpc_cidr    = var.vpc_cidr
+  source     = "./modules/vpc"
+  vpc_cidr   = var.vpc_cidr
   subnet_cidr = var.subnet_cidr
 }
 
+module "security" {
+  source = "./modules/security"
+}
+
+module "ec2" {
+  source        = "./modules/ec2"
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = module.vpc.subnet_id
+  security_group_ids = [module.security.sg_id]
+}
